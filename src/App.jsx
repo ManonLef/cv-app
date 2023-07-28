fd688d8656e5ab06ab9636cef1c77c7de8af283c
Manon <manonlefkos@gmail.com>
2023-07-28 23:03:04 +0200

    add work section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./styles/App.css";
 import GeneralInfo from "./components/General";
 import EducationSection from "./components/Educational";
+import WorkSection from "./components/Work";
 
 function App() {
   return (
     <>
       <GeneralInfo />
       <EducationSection />
+      <WorkSection />
     </>
   );
 }
