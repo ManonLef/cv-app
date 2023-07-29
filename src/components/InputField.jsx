b9de3d8c56662e79f4bd0a77daa6e3331ab6fd95
Manon <manonlefkos@gmail.com>
2023-07-29 16:12:23 +0200

    wrap input field in div

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,14 +1,16 @@
 export default function InputField({type = "text", label, name, value, onChange}) {
 
   return (
-    <label htmlFor={label}>
-      {name + ":"}
-      <input
-        type={type}
-        id={label}
-        value={value}
-        onChange={onChange}
-      />
-    </label>
+    <div>
+      <label htmlFor={label}>
+        {name + ":"}
+        <input
+          type={type}
+          id={label}
+          value={value}
+          onChange={onChange}
+        />
+      </label>
+    </div>
   );
 }
